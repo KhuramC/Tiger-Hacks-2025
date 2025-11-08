a3c6b1655e31065e4236070f8efbb592608339fc
SidKuch-gif <sidkuch555@gmail.com>
2025-11-07 23:44:52 -0600

    Resolve stash pop conflicts in world scene and Godot metadata.

--- Scripts/Pawns/Types/character.gd
+++ Scripts/Pawns/Types/character.gd
@@ -1,7 +1,11 @@
 class_name Character
 extends Pawn
 
-@export var speed: float = 1.5
+@export var speed: float = 10
+
+var max_health: int = 100
+var health: int = max_health
+@onready var health_bar: ProgressBar = $HealthBar
 
 var move_tween: Tween
 var is_moving: bool = false
@@ -9,6 +13,23 @@ var is_talking: bool = false
 
 @onready var chara_skin: Sprite2D = $Skin
 @onready var Grid: Node2D = get_parent()
+
+func _ready():
+	update_health_bar()
+
+func heal(amount: int) -> void:
+	health += amount
+	health = clamp(health, 0, max_health)
+	update_health_bar()
+
+func update_health_bar() -> void:
+	if health_bar:
+		# Use the custom function defined in progressbar.gd
+		health_bar.set_health(health, max_health)
+
+func die() -> void:
+	print(name, "has died")
+	queue_free()  # You can replace this with respawn logic later
 
 func can_move() -> bool:
 	return not is_moving and not is_talking
@@ -29,3 +50,4 @@ func _move_tween_done() -> void:
 
 func set_talking(talk_state: bool) -> void:
 	is_talking = talk_state
+	
